822278ba4c6bdb14e12ca09f8efe6032dd1198f5
rishabh kumar maurya <113686961+rishabhmaurya186@users.noreply.github.com>
2024-04-23 22:19:50 +0530

    Update auth-router.js

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const userValidation = require("../models/user-Schema")
-const jwtHelper = require('../middleware/jwt')
+//const jwtHelper = require('../middleware/jwt')
 const authController = require('../controller/auth-controller')
 
 
 router.post("/register",userValidation.RegistationData,authController.registration)
 router.post("/login",userValidation.LoginData,authController.login)
-router.use(jwtHelper.auth)
+//router.use(jwtHelper.auth)
 router.get("/",authController.getData)
 
 router.put("/",authController.byCar)
